7032b9354c25a6e116553ae0a6d4a6699c827d48
uiop <uiop@wasdhjkl.xyz>
2026-08-07 23:50:41 -0400

    chore: nix flake dev shell

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+    }:
+    let
+      eachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = eachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              nasm
+              gcc
+              gnumake
+              gdb
+              qemu
+            ];
+          };
+        }
+      );
+    };
+}
