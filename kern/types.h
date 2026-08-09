2790df5982f9711bbe62c20a8f822cbc6f26557b
uiop <uiop@wasdhjkl.xyz>
2026-08-09 16:49:14 -0400

    feat(kern): PAGE_SIZE & PAGE_ALIGN_UP macros

--- kern/types.h
+++ kern/types.h
@@ -12,6 +12,9 @@
 #define __RESERVED(n) char _reserved_##n[n]
 #define STATIC_ASSERT(cond) _Static_assert(cond, "STATIC_ASSERT: " #cond)
 
+#define PAGE_SIZE 0x1000UL // 4KiB
+#define PAGE_ALIGN_UP(x) (((x) + (PAGE_SIZE) - 1) & ~((PAGE_SIZE) - 1))
+
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
