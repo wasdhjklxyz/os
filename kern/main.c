05f278985da2d4deac6d192332b8c90d93bb1e45
uiop <uiop@wasdhjkl.xyz>
2026-09-15 01:30:53 -0400

    feat(kern): map and enter user program
    Map a fresh RW|US range at va 0 sized to the user image, read the
    image from disk into it, and iretq into it with the stack one page
    up. Replaces the old scheme of reading into USER_OFFSET before paging
    was up, and revives the commented-out __enter_user_mode. FIXME: the
    mapping is writable and executable.

--- kern/main.c
+++ kern/main.c
@@ -9,12 +9,11 @@
 #include "descriptors.h"
 #include "events.h"
 #include "io.h"
+#include "paging.h"
 #include "pm.h"
 #include "serial.h"
 #include "syscall.h"
 #include "vm.h"
-
-#define USER_STACK (USER_OFFSET + 0x100000) // FIXME: Remove this bs
 
 uint8_t kern_stack[KERN_STACK_SIZE] __attribute__((aligned(16)));
 
@@ -23,7 +22,6 @@ static void __init(void) {
   descriptors_init();
   io_disable_pic();
   syscall_init();
-  io_ata_pio_read(USER_LBA, USER_SECTORS, (uint32_t *)USER_OFFSET);
 
   const struct pm_region *physmap = pm_init();
   if (!physmap)
@@ -35,25 +33,36 @@ static void __init(void) {
   events_init();
 };
 
-// static void __enter_user_mode(void) {
-//   asm("movq %0, %%rax\n\t"
-//       "movw %%ax, %%ds\n\t"
-//       "movw %%ax, %%es\n\t"
-//       "movw %%ax, %%fs\n\t"
-//       "movw %%ax, %%gs\n\t"
-//       "pushq %0\n\t"
-//       "pushq %1\n\t"
-//       "pushq $0x202\n\t"
-//       "pushq %2\n\t"
-//       "pushq %3\n\t"
-//       "iretq"
-//       :
-//       : "r"((uint64_t)GDT_USER_DATA_SEL), "r"((uint64_t)USER_STACK),
-//         "r"((uint64_t)GDT_USER_CODE_SEL), "r"((uint64_t)USER_OFFSET)
-//       : "rax", "memory");
-// }
+static int __enter_user_mode(void) {
+  const uintptr_t va = 0;
+
+  // FIXME: The flags => executable AND read/write. Need I say more?
+  if (vm_map_range(va, USER_OFFSET,
+                   PAGE_ALIGN_UP(USER_SECTORS * DISK_BLOCK_SIZE),
+                   PTTE_RW | PTTE_US) < 0)
+    return -1;
+  io_ata_pio_read(USER_LBA, USER_SECTORS, (uint32_t *)va);
+
+  asm("movq %0, %%rax\n\t"
+      "movw %%ax, %%ds\n\t"
+      "movw %%ax, %%es\n\t"
+      "movw %%ax, %%fs\n\t"
+      "movw %%ax, %%gs\n\t"
+      "pushq %0\n\t"
+      "pushq %1\n\t"
+      "pushq $0x202\n\t"
+      "pushq %2\n\t"
+      "pushq %3\n\t"
+      "iretq"
+      :
+      : "r"((uint64_t)GDT_USER_DATA_SEL), "r"((uint64_t)(va + PAGE_SIZE)),
+        "r"((uint64_t)GDT_USER_CODE_SEL), "r"((uint64_t)va)
+      : "rax", "memory");
+
+  return 0;
+}
 
 void kern_main(void) {
   __init();
-  // __enter_user_mode();
+  __enter_user_mode();
 }
